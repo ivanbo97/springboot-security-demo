4ea7a18cad141d0bc4f078f9ab8e1954f30e05b8
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-04-02 14:24:34 +0300

    Adding methods for getting granted authorities.

--- src/main/java/com/springsecurity/course/security/role/ApplicationUserRole.java
+++ src/main/java/com/springsecurity/course/security/role/ApplicationUserRole.java
@@ -1,13 +1,19 @@
 package com.springsecurity.course.security.role;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import com.google.common.collect.Sets;
 import static com.springsecurity.course.security.role.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
 
 	STUDENT(Sets.newHashSet()),
-	ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE));
+	ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE)),
+	ADMINTRAINEE(Sets.newHashSet(COURSE_READ,STUDENT_READ));
 	
 	private final Set<ApplicationUserPermission> permissions;
 
@@ -17,5 +23,14 @@ public enum ApplicationUserRole {
 	
 	public Set<ApplicationUserPermission> getPermissions() {
 		return permissions;
+	}
+	
+	public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+		
+	Set<SimpleGrantedAuthority>	permissions= getPermissions().stream()
+			.map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+			.collect(Collectors.toSet());
+	permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+	return permissions;
 	}
 }
