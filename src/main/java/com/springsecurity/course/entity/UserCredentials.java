1c2e652d4a736cfbe4d7a2ec64bf92c5f32c489c
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-04-06 15:12:08 +0300

    Entity storing user's credentials.

---
+++ src/main/java/com/springsecurity/course/entity/UserCredentials.java
@@ -0,0 +1,127 @@
+package com.springsecurity.course.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.springsecurity.course.security.role.ApplicationUserRole;
+
+import lombok.Builder;
+
+@Entity
+@Table(name = "user_credentials")
+@Builder
+public class UserCredentials {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Enumerated(value=EnumType.STRING)
+	@Column(name = "user_role")
+	private ApplicationUserRole user_role;
+
+	public UserCredentials() {
+		
+	}
+	
+	public UserCredentials(int id, String username, String password, ApplicationUserRole user_role) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.user_role = user_role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public ApplicationUserRole getUser_role() {
+		return user_role;
+	}
+
+	public void setUser_role(ApplicationUserRole user_role) {
+		this.user_role = user_role;
+	}
+
+	@Override
+	public String toString() {
+		return "UserCredentials [id=" + id + ", username=" + username + ", password=" + password + ", user_role="
+				+ user_role + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((user_role == null) ? 0 : user_role.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserCredentials other = (UserCredentials) obj;
+		if (id != other.id)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (user_role == null) {
+			if (other.user_role != null)
+				return false;
+		} else if (!user_role.equals(other.user_role))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
